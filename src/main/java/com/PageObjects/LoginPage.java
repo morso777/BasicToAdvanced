7fc34a8cafa3e09d109e1ab196ecce99c9bc3993
Hiran pena toledo <hiran.penatoledo@finning.com>
2023-04-14 14:01:15 -0600

    TC login negative

--- src/main/java/com/PageObjects/LoginPage.java
+++ src/main/java/com/PageObjects/LoginPage.java
@@ -9,7 +9,10 @@ public class LoginPage extends BasePage{
     String password ="secret_sauce";
     String userName="standard_user";
 
-    By byUserName= By.id("user-name");
+    //By byUserName= By.id("user-name");
+    By byUserName= By.cssSelector("input[data-test='username']");
+    //By byUserName= By.xpath("//input[@data-test='username']");
+    //input[data-test="username"]
     By byPassword= By.id("password");
     By byLogin=By.id("login-button");
 
@@ -20,6 +23,15 @@ public class LoginPage extends BasePage{
     public void Login (){
         WebElement userName= driver.findElement(byUserName);
         userName.sendKeys("standard_user");
+        WebElement password= driver.findElement(byPassword);
+        password.sendKeys("secret_sauce");
+        WebElement login= driver.findElement(byLogin);
+        login.click();
+    }
+
+    public void loginNegative (){
+        WebElement userName= driver.findElement(byUserName);
+        userName.sendKeys("s");
         WebElement password= driver.findElement(byPassword);
         password.sendKeys("secret_sauce");
         WebElement login= driver.findElement(byLogin);
